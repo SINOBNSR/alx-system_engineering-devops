1daa123b923abac9fdc2bb2c858777015a9f1eea
SINOBNSR <yassine.bnsr@gmail.com>
2023-02-16 15:06:06 -0800

    xtra 2

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unstid.h>
+/**
+ * main - prints to string
+ * Return: 1
+ */
+int main (void)
+{
+fputs("add that piece of art is useful\" - Dora korpar, 2015-10-19\n", stdout);
+return (1);
+}
