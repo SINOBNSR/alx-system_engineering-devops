6e5bf9cd84b56728ceebe34b320a8db7d10fff5b
SINOBNSR <yassine.bnsr@gmail.com>
2023-02-16 13:39:47 -0800

    task5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <studio.h>
+/**
+ * main - A program that writes using the printf function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+		return (0);
+}
