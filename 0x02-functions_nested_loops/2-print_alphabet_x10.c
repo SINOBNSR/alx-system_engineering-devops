a081175368787aea1b82c19cd41c94069f776901
SINOBNSR <yassine.bnsr@gmail.com>
2023-02-21 08:25:15 -0800

    10xalphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Entry point
+ * Description: prints the alphabet x10 times in lowercase,
+ * followed by a new line.
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	char x;
+	int i = 0;
+
+	while (i <= 9)
+	{
+		for (x = 'a'; x <= 'z'; x++)
+		{
+			_putchar(x);
+		}
+		_putchar('\n');
+		x++;
+	}
+}
