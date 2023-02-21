a4e0988cb2f5931cabc82791a1d54f9658b36bdd
SINOBNSR <yassine.bnsr@gmail.com>
2023-02-21 15:59:29 -0800

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_last_digit - Entry point
+ * Description: prints the last digit of a number
+ * @n: is an integer
+ * Return: integer
+ */
+int print_last_digit(int n)
+{
+	int k;
+
+	k = n % 10;
+	if (n < 10)
+		k = -k;
+	return (n);
+}
