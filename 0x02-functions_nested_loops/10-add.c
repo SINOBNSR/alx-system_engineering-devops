d68a72f86f54c47098e53463f6af3628f9dee75a
SINOBNSR <yassine.bnsr@gmail.com>
2023-02-21 17:45:56 -0800

    +

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * add - adds two numbers
+ * Description: prints sums of two integers
+ * @a: is a number
+ * @b: is a number
+ *
+ * Return: sumvalue
+ */
+int add(int a, int b)
+{
+	int sumvalue;
+
+	sumvalue = a + b;
+	return (sumvalue);
+}
