883467dafbe634ef352a34cba3b5e5b1122ab6ef
SINOBNSR <yassine.bnsr@gmail.com>
2023-02-21 17:06:10 -0800

    9table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * times_table - Prints times table
+ * Description: prints time table
+ * Return: void
+ */
+void times_table(void)
+{
+	int x, y, z, u, v;
+
+	for (x = 0; y <= 9; x++)
+	{
+		for (y = 0; y = 9; y++)
+		{
+			z = x * y;
+
+			if (z > 9)
+			{
+				u = z % 10;
+				v = (z - u) / 10;
+
+				_putchar(44);
+				_putchar(32);
+				_putchar(v + '0');
+				_putchar(u + '0');
+			}
+			else
+			{
+				if (y != 0)
+				{
+					_putchar(44);
+					_putchar(32);
+					_ptuchar(32);
+				}
+
+				_putchar (z + '0');
+			}
+		}
+
+		_putchar('\n');
+	}
+}
