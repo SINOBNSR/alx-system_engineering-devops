4d6b8ad371b5f47d5e4a3d1360f6c3a37cf63ded
SINOBNSR <yassine.bnsr@gmail.com>
2023-02-23 09:03:48 -0800

    upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+/**
+ * _isupper - checks if a character is an uppercase
+ * @c: character to check
+ * Return: 1 if true, 0 if false
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	return (0);
+}
