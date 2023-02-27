cd1b3b13c58489f1c91cf013dc9b2db71746e59e
SINOBNSR <yassine.bnsr@gmail.com>
2023-02-27 08:56:01 -0800

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts2 - pritnts every other string
+ * @str: referenced string
+ * Return: 0
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		i++;
+	}
+	_putchar('\n');
+}
